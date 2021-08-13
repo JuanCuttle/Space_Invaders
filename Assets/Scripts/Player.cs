311e17f593d68b045795bdadf15558424050eec5
JuanCuttle <jatcuttle@yahoo.com.br>
2021-08-13 17:35:41 -0300

    Game over added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,11 +14,24 @@ public class Player : MonoBehaviour
 
 	public int points;
 
+	public int lives = 3;
+
+	private bool gameIsPaused;
+
 	// Update is called once per frame
 	void Update()
 	{
 
-		//print(this.points);
+		if (this.gameIsPaused)
+        {
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				Time.timeScale = 1f;
+				this.gameIsPaused = false;
+				SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+			}
+		}
+
 		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
 		{
 			this.transform.position += Vector3.left * this.speed * Time.deltaTime;
@@ -59,12 +72,24 @@ public class Player : MonoBehaviour
 		if (other.gameObject.layer == LayerMask.NameToLayer("Invader") ||
 			other.gameObject.layer == LayerMask.NameToLayer("Missile"))
 		{
-			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+			this.lives--;
+			this.points -= 20;
+			if (this.lives <= 0)
+            {
+				Time.timeScale = 0f;
+				this.gameIsPaused = true;
+			}
 		}
 	}
 
 	private void OnGUI()
 	{
 		GUI.Label(new Rect(10, 10, 100, 20), "Pontos: " + this.points);
+		GUI.Label(new Rect(350, 10, 100, 20), "Vidas: " + this.lives);
+
+		if (this.lives <= 0)
+        {
+			GUI.Label(new Rect(170, 200, 1000, 1000), "GAME OVER");
+		}
 	}
 }
